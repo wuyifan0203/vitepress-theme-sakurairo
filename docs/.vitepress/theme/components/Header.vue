999ec91774e3690c9babb17bfad6e46d76b29ef4
wuyifan <1208097313@qq.com>
2024-02-10 20:55:01 +0800

    feat:完成初步搭建

---
+++ docs/.vitepress/theme/components/Header.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: wuyifan 1208097313@qq.com
+ * @Date: 2024-02-10 20:30:35
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2024-02-10 20:49:11
+ * @FilePath: /vuepress-interview/docs/.vitepress/theme/components/Header.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <header>
+        
+
+
+    </header>
+</template>
+    
+<script setup lang='ts'>
+
+</script>
+    
+<style></style>
---
+++ docs/.vitepress/theme/layout/Layout.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: wuyifan 1208097313@qq.com
+ * @Date: 2024-02-10 20:22:24
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2024-02-10 20:35:25
+ * @FilePath: /vuepress-interview/docs/theme/layout/Layout.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <Header></Header>
+    <main></main>
+    <div>layout</div>
+</template>
+    
+<script setup lang='ts'>
+import Header from '../components/Header.vue';
+    
+</script>
+    
+<style>
+    
+</style>
