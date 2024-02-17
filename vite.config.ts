c31dc2da7efab7743de6c58e475dfe66afab0956
wuyifan <1208097313@qq.com>
2024-02-18 01:48:45 +0800

    feat:初步完成文章部分2

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: wuyifan 1208097313@qq.com
+ * @Date: 2024-02-17 23:25:49
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2024-02-17 23:26:34
+ * @FilePath: /vuepress-interview/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE)
+ */
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    
+})
