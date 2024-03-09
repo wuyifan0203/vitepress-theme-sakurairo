ddb86b85b0cf780d0f0eb3ab5c3eaccb2752140c
wuyifan <1208097313@qq.com>
2024-03-09 23:12:06 +0800

    wip: 📖 store

---
+++ src/utils/store.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: wuyifan 1208097313@qq.com
+ * @Date: 2024-03-09 23:10:25
+ * @LastEditors: wuyifan 1208097313@qq.com
+ * @LastEditTime: 2024-03-09 23:11:01
+ * @FilePath: /vuepress-interview/src/utils/store.ts
+ * Copyright (c) 2024 by wuyifan0203 email: 1208097313@qq.com, All Rights Reserved.
+ */
+// 全局变量存储
+const createStore = () => {
+    
+}
+
+
+export { createStore }
