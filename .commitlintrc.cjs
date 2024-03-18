0a02b007ef5a157efb570f2d04fc5f7f909919ef
wuyifan0203 <1208097313@qq.com>
2024-03-18 14:06:25 +0800

    ci: :ferris_wheel: 更新commitlint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -2,7 +2,7 @@
  * @Author: wuyifan0203 1208097313@qq.com
  * @Date: 2024-03-18 13:16:16
  * @LastEditors: wuyifan0203 1208097313@qq.com
- * @LastEditTime: 2024-03-18 13:48:26
+ * @LastEditTime: 2024-03-18 14:03:56
  * @FilePath: /vitepress-theme-sakurairo/.commitlintrc.cjs
  * Copyright (c) 2024 by wuyifan0203 email: 1208097313@qq.com, All Rights Reserved.
  */
@@ -25,19 +25,19 @@ module.exports = {
             confirmCommit: '是否提交或修改commit ?'
         },
         types: [
-            { value: 'feat', name: 'feat:     新增功能 | A new feature' },
-            { value: 'fix', name: 'fix:      修复缺陷 | A bug fix' },
-            { value: 'docs', name: 'docs:     文档更新 | Documentation only changes' },
-            { value: 'style', name: 'style:    代码格式 | Changes that do not affect the meaning of the code' },
-            { value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature' },
-            { value: 'perf', name: 'perf:     性能提升 | A code change that improves performance' },
-            { value: 'test', name: 'test:     测试相关 | Adding missing tests or correcting existing tests' },
-            { value: 'build', name: 'build:    构建相关 | Changes that affect the build system or external dependencies' },
-            { value: 'ci', name: 'ci:       持续集成 | Changes to our CI configuration files and scripts' },
-            { value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
-            { value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files' },
+            { value: 'feat', name: 'feat:     新增功能 | A new feature', emoji: ':sparkles:' },
+            { value: 'fix', name: 'fix:      修复缺陷 | A bug fix',emoji: ':bug:' },
+            { value: 'docs', name: 'docs:     文档更新 | Documentation only changes' , emoji: ':memo:'},
+            { value: 'style', name: 'style:    代码格式 | Changes that do not affect the meaning of the code' ,emoji: ':lipstick:'},
+            { value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature', emoji: ':recycle:' },
+            { value: 'perf', name: 'perf:     性能提升 | A code change that improves performance', emoji: ':zap:' },
+            { value: 'test', name: 'test:     测试相关 | Adding missing tests or correcting existing tests', emoji: ':white_check_mark:' },
+            { value: 'build', name: 'build:    构建相关 | Changes that affect the build system or external dependencies', emoji: ':package:' },
+            { value: 'ci', name: 'ci:       持续集成 | Changes to our CI configuration files and scripts', emoji: ':ferris_wheel:' },
+            { value: 'revert', name: 'revert:   回退代码 | Revert to a commit', emoji: ':rewind:' },
+            { value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files', emoji: ':hammer:' },
         ],
-        useEmoji: false,
+        useEmoji: true,
         emojiAlign: 'center',
         useAI: false,
         aiNumber: 1,
