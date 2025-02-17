ae87e8c6bff36f54a9ffcfbda27d25482d86f28e
wuyifan0203 <1208097313@qq.com>
2025-02-17 14:17:45 +0800

    refactor: 💡 rebuild

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,18 +2,18 @@
  * @Author: wuyifan0203 1208097313@qq.com
  * @Date: 2024-02-10 20:17:09
  * @LastEditors: wuyifan0203 1208097313@qq.com
- * @LastEditTime: 2024-03-14 17:38:28
- * @FilePath: /vitepress-theme-sakurairo/docs/.vitepress/theme/index.ts
+ * @LastEditTime: 2025-02-17 14:18:27
+ * @FilePath: \vitepress-theme-sakurairo\docs\.vitepress\theme\index.ts
  */
-import { Awaitable, EnhanceAppContext, Theme } from 'vitepress';
-import { Layout } from '../../../src';
-
+import { Awaitable, EnhanceAppContext, Theme } from "vitepress";
+import { Layout, initTheme } from "../../../src";
 
 const themeConfig: Theme = {
-    Layout,
-    enhanceApp: (ctx: EnhanceAppContext): Awaitable<void> => {
-    },
-    extends: undefined
+  Layout,
+  enhanceApp: (ctx: EnhanceAppContext): Awaitable<void> => {
+    initTheme(ctx);
+  },
+  extends: undefined,
 };
 
 export default themeConfig;
